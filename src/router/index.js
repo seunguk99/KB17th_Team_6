29666474ec2b69fed9560f81eda57d255eb8ce57
seunguk99 <seunguk99@gmail.com>
2025-04-07 17:35:17 +0900

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '@/pages/HomePage.vue';
+import TransactionHistory from '@/pages/TransactionHistory.vue';
+import TransactionEditPage from '@/pages/TransactionEditPage.vue';
+import TransactionWritePage from '@/pages/TransactionWritePage.vue';
+import DetailTransactionPage from '@/pages/DetailTransactionPage.vue';
+import ProfilePage from '@/pages/ProfilePage.vue';
+import EditProfilePage from '@/pages/EditProfilePage.vue';
+import UserSelection from '@/pages/UserSelection.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', component: UserSelection },
+    { path: '/home', component: HomePage },
+    { path: '/transactions', component: TransactionHistory },
+    { path: '/transactions/new', component: TransactionWritePage },
+    { path: '/transactions/:id', component: TransactionEditPage },
+    { path: '/transactions/:id/edit', component: DetailTransactionPage },
+    { path: '/profile', component: ProfilePage },
+    { path: '/profile/edit', component: EditProfilePage },
+  ],
+});
+
+export default router;
