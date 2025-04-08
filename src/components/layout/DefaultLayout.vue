b9fae74d6839337f6d280a9e7efabd441499bb5c
Minyoung06 <monica_y@naver.com>
2025-04-08 10:57:38 +0900

    Feat: 레이아웃 설정
    추가된 사항
    - Bootstrap5, Fontawesome 적용
    - 기본 레이아웃 요소들 배치

---
+++ src/components/layout/DefaultLayout.vue
@@ -0,0 +1,11 @@
+<!-- ./components/layouts/DefaultLayout.vue -->
+<template>
+  <div class="container">
+    <slot name="header"></slot>
+    <slot name="navbar"></slot>
+    <div class="content my-5 px-3">
+      <slot></slot>
+    </div>
+    <slot name="footer"></slot>
+  </div>
+</template>
