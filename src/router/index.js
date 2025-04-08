6d3a69221c7eccd61e32e1ac9c3bf6e6580c98c2
Minyoung06 <monica_y@naver.com>
2025-04-08 14:50:28 +0900

    Refactor: routes 이름 추가

--- src/router/index.js
+++ src/router/index.js
@@ -11,35 +11,51 @@ import UserSelection from '@/pages/UserSelection.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: UserSelection, meta: { title: '로그인' } },
-    { path: '/home', component: HomePage, meta: { title: 'Home' } },
+    {
+      path: '/',
+      name: 'userSelection',
+      component: UserSelection,
+      meta: { title: '로그인' },
+    },
+    {
+      path: '/home',
+      name: 'homePage',
+      component: HomePage,
+      meta: { title: 'Home' },
+    },
     {
       path: '/transactions',
+      name: 'transactionHistory',
       component: TransactionHistory,
       meta: { title: '거래 내역' },
     },
     {
       path: '/transactions/new',
+      name: 'transactionWritePage',
       component: TransactionWritePage,
       meta: { title: '거래 내역 등록' },
     },
     {
       path: '/transactions/:id',
+      name: 'detailTransactionPage',
       component: DetailTransactionPage,
       meta: { title: '상세 거래 내역' },
     },
     {
       path: '/transactions/:id/edit',
+      name: 'transactionEditPage',
       component: TransactionEditPage,
       meta: { title: '거래 내역 수정' },
     },
     {
       path: '/profile',
+      name: 'profilePage',
       component: ProfilePage,
       meta: { title: '설정 및 프로필' },
     },
     {
       path: '/profile/edit',
+      name: 'editProfilePage',
       component: EditProfilePage,
       meta: { title: '프로필 수정' },
     },
