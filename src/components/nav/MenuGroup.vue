d1a083d906105a109676e9fdc54c12949892d641
Minyoung06 <monica_y@naver.com>
2025-04-08 16:31:59 +0900

    Feat: NavBar
    추가된 사항
    - navbar 기능 추가
    - navbar에는 홈, 거래내역, 거래내역 등록만 우선 넣음
    - NavBar.vue에서 메뉴 추가 가능

---
+++ src/components/nav/MenuGroup.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul class="navbar-nav flex-row justify-content-around w-100">
+    <li class="nav-item" v-for="item in items" :key="item.name">
+      <MenuItem :name="item.name" />
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import MenuItem from './MenuItem.vue';
+
+defineProps({
+  items: Array,
+});
+</script>
---
+++ src/components/nav/NavBar.vue
@@ -0,0 +1,17 @@
+<script setup>
+import MenuGroup from './MenuGroup.vue';
+
+const navMenus = [
+  { name: 'homePage' },
+  { name: 'transactionHistory' },
+  { name: 'transactionWritePage' },
+];
+</script>
+
+<template>
+  <nav class="navbar bg-light border-end py-2 px-4">
+    <div class="container-fluid">
+      <MenuGroup :items="navMenus" />
+    </div>
+  </nav>
+</template>
