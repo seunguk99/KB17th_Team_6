45a7d456dfa89793ae546c3c93857902f3c50be7
Minyoung06 <monica_y@naver.com>
2025-04-09 10:06:38 +0900

    Feat: User Profile Edit Page
    추가된 사항
    - 유저 정보 수정 페이지 구현

--- src/pages/EditProfilePage.vue
+++ src/pages/EditProfilePage.vue
@@ -1,3 +1,58 @@
 <template>
-  <h1>Edit Profile Page</h1>
+  <div class="container py-5">
+    <h2 class="mb-4">프로필 수정</h2>
+
+    <form class="card p-4" @submit.prevent="saveChange">
+      <div class="mb-3">
+        <label for="name" class="form-label"
+          >이름<span class="text-danger">*</span></label
+        >
+        <input
+          v-model="editUser.name"
+          type="text"
+          id="name"
+          class="form-control"
+          required
+        />
+
+        <label for="email" class="form-label"
+          >이메일<span class="text-danger">*</span></label
+        >
+        <input
+          v-model="editUser.email"
+          type="email"
+          id="email"
+          class="form-control"
+          required
+        />
+      </div>
+
+      <button type="submit" class="btn btn-primary">저장</button>
+    </form>
+  </div>
 </template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useUserStore } from '@/stores/userStore';
+
+const router = useRouter();
+const userStore = useUserStore();
+
+const editUser = ref({
+  name: '',
+  email: '',
+});
+
+onMounted(() => {
+  if (userStore.currentUser) {
+    editUser.value = { ...userStore.currentUser };
+  }
+});
+
+const saveChange = async () => {
+  await userStore.updateUser(editUser.value);
+  router.push({ name: 'profilePage' });
+};
+</script>
